663a23d703f9a6523af71a98d7e2ad8be8a035ed
YCY <app97204@gmail.com>
2023-07-22 22:54:47 +0800

    first create

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request
+
+import json
+import os
+
+# 載入 LINE Message API 相關函式庫
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+app = Flask(__name__)
+
+@app.route("/callback", methods=['POST'])
+def linebot():
+    body = request.get_data(as_text=True)                    # 取得收到的訊息內容
+    try:
+        json_data = json.loads(body)                         # json 格式化訊息內容
+        access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None) #'xlZdjV8xBe1KnFb6Y2s3VAUgIIWHsAz7M/A5XfD4OROyD7aNP2TlDXbi2zOEr4ViA7vAf0sF1M9tWlNgQ7Vgn2OizwRIPOsrvegS/xZRDjIyl3FoJTef59QGEboERKHvLSHECClHfUcelFpYid8R2QdB04t89/1O/w1cDnyilFU='
+        secret = os.getenv('LINE_CHANNEL_SECRET', None) #'b72e75a73bbe5227a1ed39d1c3bd7706'
+        line_bot_api = LineBotApi(access_token)              # 確認 token 是否正確
+        handler = WebhookHandler(secret)                     # 確認 secret 是否正確
+        signature = request.headers['X-line-signature']      # 加入回傳的 headers
+        handler.handle(body, signature)                      # 綁定訊息回傳的相關資訊
+        tk = json_data['events'][0]['replyToken']            # 取得回傳訊息的 Token
+        type = json_data['events'][0]['message']['type']     # 取得 LINe 收到的訊息類型
+        if type=='text':
+            msg = json_data['events'][0]['message']['text']  # 取得 LINE 收到的文字訊息
+            print(msg)                                       # 印出內容
+            reply = f'你告訴我：{msg}'
+        else:
+            reply = '你傳的不是文字呦～'
+        print(reply)
+        line_bot_api.reply_message(tk,TextSendMessage(reply))# 回傳訊息
+    except:
+        print(body)                                          # 如果發生錯誤，印出收到的內容
+    return 'OK'                                              # 驗證 Webhook 使用，不能省略
+
+if __name__ == "__main__":
+    app.run(port=8080)
